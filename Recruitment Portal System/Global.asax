400bb4a1ecdf877388dc33c4517146147005d5d9
Fecs.Prince <fecs.prince@gmail.com>
2023-01-16 14:55:56 +0100

    Add project files.

---
+++ Recruitment Portal System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Recruitment_Portal_System.MvcApplication" Language="C#" %>
---
+++ Recruitment Portal System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Recruitment_Portal_System.Startup))]
+namespace Recruitment_Portal_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
